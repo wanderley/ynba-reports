ea74887341ce642b893b910dac066d4a38f81717
Wanderley da Silva <wanderley.guimaraes@gmail.com>
2022-12-19 22:02:15 -0800

    sort aggregated results (outflow)

--- report.rkt
+++ report.rkt
@@ -8,19 +8,18 @@
 ;; Example of report:
 ;;
 ;;     *Top outflow transactions:*
+;;     - G2 = $1500
+;;       - C2 = $600
+;;       - C1 = $300
+;;       - C3 = $600
 ;;     - G1 = $1000
-;;       - C1 = $300
 ;;       - C2 = $600
-;;       - C3 = $80
-;;       - Others = $20
-;;     - G2 = $1000
 ;;       - C1 = $300
-;;       - C2 = $600
 ;;       - C3 = $80
 ;;       - Others = $20
 ;;     - G3 = $1000
-;;       - C1 = $300
 ;;       - C2 = $600
+;;       - C1 = $300
 ;;       - C3 = $80
 ;;       - Others = $20
 ;;     - Others = $1000
@@ -45,19 +44,21 @@
                  (string->number (string-replace (list-ref line 9) "$" "")))))
 
 (module+ test
-  (check-equal? (aggregate-outflow (list (transaction "G1" "C1" 15.0 0.0)
-                                         (transaction "G2" "C1" 10.0 0.0)
+  (check-equal? (aggregate-outflow (list (transaction "G2" "C1" 15.0 0.0)
+                                         (transaction "G1" "C1" 10.0 0.0)
                                          (transaction "G3" "C1"  0.0 7.0)
                                          (transaction "G4" "C2"  1.0 0.0)
                                          (transaction "G4" "C2"  0.0 1.0)
                                          (transaction "G5" "C3"  0.0 5.0)
-                                         (transaction "G1" "C1" 15.0 0.0)))
-                '(("G1" . 30.0) ("G2" . 10.0) ("G4" . 1.0))))
+                                         (transaction "G2" "C1" 15.0 0.0)))
+                '(("G2" . 30.0) ("G1" . 10.0) ("G4" . 1.0))))
 (define (aggregate-outflow ts)
-  (filter (λ (a) (> (cdr a) 0))
-          (for/list ([g (group-by-transaction-group ts)])
-            (cons (transaction-group (first g))
-                  (for/sum ([t g]) (transaction-outflow t))))))
+  (sort
+   (filter (λ (a) (> (cdr a) 0))
+           (for/list ([g (group-by-transaction-group ts)])
+             (cons (transaction-group (first g))
+                   (for/sum ([t g]) (transaction-outflow t)))))
+   >= #:key cdr))
 
 (define (group-by-transaction-group ts)
   (for/fold ([groups '()]
