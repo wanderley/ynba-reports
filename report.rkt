d2939b6ce9f08e9cdba13bcc305fcfc831fb83eb
Wanderley da Silva <wanderley.guimaraes@gmail.com>
2022-12-19 21:13:55 -0800

    implement aggregation of groups by outflow

--- report.rkt
+++ report.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+(module+ test
+  (require rackunit))
+
 ;; Given a list of transaction, print a report.
 ;;
 ;; Example of report:
@@ -41,5 +44,34 @@
                  (string->number (string-replace (list-ref line 8) "$" ""))
                  (string->number (string-replace (list-ref line 9) "$" "")))))
 
-(csv->transactions
- (read-csv "example.csv"))
+(module+ test
+  (check-equal? (aggregate-outflow (list (transaction "G1" "C1" 15.0 0.0)
+                                         (transaction "G2" "C1" 10.0 0.0)
+                                         (transaction "G3" "C1"  0.0 7.0)
+                                         (transaction "G4" "C2"  1.0 0.0)
+                                         (transaction "G4" "C2"  0.0 1.0)
+                                         (transaction "G5" "C3"  0.0 5.0)
+                                         (transaction "G1" "C1" 15.0 0.0)))
+                '(("G1" . 30.0) ("G2" . 10.0) ("G4" . 1.0))))
+(define (aggregate-outflow ts)
+  (filter (λ (a) (> (cdr a) 0))
+          (for/list ([g (group-by-transaction-group ts)])
+            (cons (transaction-group (first g))
+                  (for/sum ([t g]) (transaction-outflow t))))))
+
+(define (group-by-transaction-group ts)
+  (for/fold ([groups '()]
+             #:result (reverse groups))
+            ([t (sort ts string<=? #:key transaction-group)])
+    (cond
+      [(empty? groups) (list (list t))]
+      [(equal? (transaction-group (first (first groups)))
+               (transaction-group t))
+       (cons (cons t (first groups))
+             (rest groups))]
+      [else
+       (cons (list t) groups)])))
+
+(aggregate-outflow
+ (csv->transactions
+  (read-csv "example.csv")))
